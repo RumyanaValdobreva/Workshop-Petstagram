fd2c36507b8b22569b8892e1cd29aecd755b423a
Rumyana Valdobreva <rumyiwanowa@abv.bg>
2024-10-14 09:41:53 +0300

    fix: fixed slug field

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram",
+        "USER": "rumyana-valdobreva",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
